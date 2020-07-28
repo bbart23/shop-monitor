adc16e7aea9e9516b38e73fb525a1b4d685c044d
bbart23 <brandonbart@me.com>
2020-07-28 17:23:28 -0400

    Added Anti Social Monitor

---
+++ antisocial_monitor.py
@@ -0,0 +1,118 @@
+import requests
+import pickle
+import discord
+import datetime
+import time
+import re
+from discord import Webhook, RequestsWebhookAdapter
+from siteItem import SiteItem
+from os import path
+
+if path.exists('antisocial_list.dat'):
+    filehandler = open('bape_list.dat', 'rb')
+    ItemList = pickle.load(filehandler)
+else:
+    ItemList = []
+
+antiSocialHook = 'https://discordapp.com/api/webhooks/737764108216041592/tJFSnD-eEE14WvMMJU4l2vOmfPBSlKXv2C0-xoT-vTwqu4JDrc8eY0ezQq2jwmuN3YXI'
+
+
+antiSocial = Webhook.from_url(antiSocialHook, adapter=RequestsWebhookAdapter())
+
+def ExistsInList(item):
+    ind = -1
+    for Item in ItemList:
+        ind += 1
+        if Item.Name == item.Name:
+            return ind
+    return -1
+
+def SendDiscordMessage(itemName, itemColor, price, availability, URL, imageURL, sizes, webhook):
+    Desc = '**Shop:** [Anti Social Social Club (US)](https://www.antisocialsocialclub.com/)\n**Price:** ' + price + '\n**State:** ' + availability + '\n\n> QuickTask\n> [' + sizes + '](' + URL + ')';
+    Color = 12726577
+    Footer = '[SneakerAlpha]'
+    embed = discord.Embed(title=itemName, description=Desc, url=URL, color=Color, timestamp=datetime.datetime.now())
+    embed.set_thumbnail(url=imageURL)
+    embed.set_footer(text=Footer)
+    webhook.send(embed=embed, username='Sneaker Alphas')
+
+pageNum = 0
+
+while True:
+    time.sleep(10)
+    pageNum += 1
+    request = requests.get('https://www.antisocialsocialclub.com/products.json?limit=250&page=' + str(pageNum))
+
+    decodedJson = request.json()
+
+    if len(decodedJson['products']) == 0:
+        pageNum = 0
+        continue
+
+    for product in decodedJson['products']:
+        webhook = antiSocial
+
+        ItemName = product['title']
+        SoldOut = True
+        sizeString = ''
+        colorString = ''
+        for variant in product['variants']:
+            #print(variant['available'])
+            if variant['available'] == True:
+                SoldOut = False
+                size = variant['option1']
+                if size not in sizeString:
+                    if sizeString == '':
+                        sizeString += size
+                    else:
+                        sizeString += '|' + size
+
+        ItemColor = colorString
+        if product['variants'][0]['featured_image'] is not None:
+            ItemPicture = product['variants'][0]['featured_image']['src']
+        else:
+            try:
+                ItemPicture = product['images'][0]['src']
+            except IndexError:
+                ItemPicture = ''
+        ItemPrice = '$' + product['variants'][0]['price']
+        ItemLink = 'https://www.antisocialsocialclub.com/products/' + product['handle']
+        temp = SiteItem(ItemName, ItemColor, SoldOut)
+
+
+        index = ExistsInList(temp)
+
+        # If item already exists in list
+        if index != -1:
+            oldSoldOut = ItemList[index].SoldOut
+
+            if SoldOut != oldSoldOut:
+                if not SoldOut:
+                    try:
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'RESTOCK', ItemLink, ItemPicture, sizeString, webhook)
+                        print('[RESTOCK]' + ItemName)
+                    except:
+                        print('ERROR: Couldn\'t send message. Product Type: '+ product['product_type'].lower())
+                else:
+                    try:
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'Sold Out', ItemLink, ItemPicture, sizeString, webhook)
+                        print('[SOLD OUT]' + ItemName)
+                    except:
+                        print('ERROR: Couldn\'t send message. Product Type: '+ product['product_type'].lower())
+                ItemList.pop(index)
+                ItemList.append(temp)
+        else:
+            if not SoldOut:
+                try:
+                    SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'In Stock', ItemLink, ItemPicture, sizeString, webhook)
+                    print('[IN STOCK]' + ItemName)
+                except:
+                    print('ERROR: Couldn\'t send message. Product Type: ' + product['product_type'].lower())
+            ItemList.append(temp)
+
+
+
+    #print(decodedJson['products'][0]['title'])
+    file = open('antisocial_list.dat', 'wb')
+    pickle.dump(ItemList, file)
+    file.close()
