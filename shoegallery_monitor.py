9b07682809a7b22932f7053782532085fb4438ac
bbart23 <brandonbart@me.com>
2020-08-11 14:12:30 -0400

    Added Shoe Gallery

---
+++ shoegallery_monitor.py
@@ -0,0 +1,157 @@
+import requests
+import pickle
+import discord
+import datetime
+import time
+import re
+from discord import Webhook, RequestsWebhookAdapter
+from siteItem import SiteItem
+from os import path
+
+if path.exists('shoegallery_list.dat'):
+    filehandler = open('shoegallery_list.dat', 'rb')
+    ItemList = pickle.load(filehandler)
+else:
+    ItemList = []
+
+shoegalleryHook = 'https://discordapp.com/api/webhooks/742804665275514970/LBvodsAs6B724HUt6aSHVR3IZL4jXcMteUfmNdIsK3zgaqMq8ClPQJuGzpDUbxm28aos'
+shoegallery = Webhook.from_url(shoegalleryHook, adapter=RequestsWebhookAdapter())
+
+jordanHook = 'https://discordapp.com/api/webhooks/739924749127516291/Q3rX5AenW1d9E7vB9BA8HBncLmk8u0Y2z3zHR9mMjKqpUZTI_sDqyRl4xM_HN9gatMJk'
+yeezyHook = 'https://discordapp.com/api/webhooks/739927902786945105/TnD1CAjmWdJHj5AEiIsbq1SmEr1kD92z6l37qCdBKTuI67_YErcgFSM8buui2cSoMNo1'
+nikesbHook = 'https://discordapp.com/api/webhooks/739927996391227493/oZdCOwhdb7uErLSiujwz452Cyja6Fg1lw_1li_25D190BmKrTuSsO0wdmW0uiG90Grip'
+
+jordan = Webhook.from_url(jordanHook, adapter=RequestsWebhookAdapter())
+yeezy = Webhook.from_url(yeezyHook, adapter=RequestsWebhookAdapter())
+nikesb = Webhook.from_url(nikesbHook, adapter=RequestsWebhookAdapter())
+
+def ExistsInList(item):
+    ind = -1
+    for Item in ItemList:
+        ind += 1
+        if Item.Name == item.Name and Item.Color == item.Color:
+            return ind
+    return -1
+
+def SendDiscordMessage(itemName, itemColor, price, availability, URL, imageURL, sizes, webhook):
+    Desc = '**Shop:** [Shoe Gallery Miami (US)](https://shoegallerymiami.com/)\n**Price:** ' + price + '\n**State:** ' + availability + '\n\n> QuickTask\n> [' + sizes + '](' + URL + ')';
+    Color = 12726577
+    Footer = '[SneakerAlpha]'
+    embed = discord.Embed(title=itemName, description=Desc, url=URL, color=Color, timestamp=datetime.datetime.now())
+    embed.set_thumbnail(url=imageURL)
+    embed.set_footer(text=Footer)
+    webhook.send(embed=embed, username='Sneaker Alphas')
+
+
+#brands = ['adidas', 'jordan', 'play', 'butter', 'nike', 'y-3', 'yeezy', 'white']
+pageNum = 0
+
+while True:
+    time.sleep(10)
+    pageNum += 1
+    request = requests.get('https://shoegallerymiami.com/products.json?limit=250&page=' + str(pageNum))
+
+    decodedJson = request.json()
+
+    if len(decodedJson['products']) == 0:
+        pageNum = 0
+        continue
+
+    for product in decodedJson['products']:
+        webhook = shoegallery
+
+        #if all(x not in product['vendor'].lower() for x in brands):
+        #    continue
+
+        ItemName = product['title']
+        ItemColor = product['handle']
+        SoldOut = True
+        sizeString = ''
+        for variant in product['variants']:
+            #print(variant['available'])
+            if variant['available'] == True:
+                SoldOut = False
+                size = variant['option1']
+                if size is None:
+                    size = 'F'
+                if size not in sizeString:
+                    if sizeString == '':
+                        sizeString += size
+                    else:
+                        sizeString += '|' + size
+
+        if product['variants'][0]['featured_image'] is not None:
+            ItemPicture = product['variants'][0]['featured_image']['src']
+        else:
+            try:
+                ItemPicture = product['images'][0]['src']
+            except IndexError:
+                ItemPicture = ''
+        ItemPrice = '$' + product['variants'][0]['price']
+        ItemLink = 'https://shoegallerymiami.com/products/' + product['handle']
+        temp = SiteItem(ItemName, ItemColor, SoldOut)
+
+
+        index = ExistsInList(temp)
+
+        # If item already exists in list
+        if index != -1:
+            oldSoldOut = ItemList[index].SoldOut
+
+            if SoldOut != oldSoldOut:
+                if not SoldOut:
+                    try:
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'RESTOCK', ItemLink, ItemPicture, sizeString, webhook)
+                        print('[RESTOCK]' + ItemName)
+                        if 'jordan' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'RESTOCK', ItemLink, ItemPicture,
+                                               sizeString, jordan)
+                        if 'yeezy' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'RESTOCK', ItemLink, ItemPicture,
+                                               sizeString, yeezy)
+                        if 'nike' in ItemName.lower() and 'sb' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'RESTOCK', ItemLink, ItemPicture,
+                                               sizeString, nikesb)
+                    except:
+                        print('ERROR: Couldn\'t send message. Product Type: '+ product['product_type'].lower())
+                else:
+                    try:
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'Sold Out', ItemLink, ItemPicture, sizeString, webhook)
+                        print('[SOLD OUT]' + ItemName)
+                        if 'jordan' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'Sold Out', ItemLink, ItemPicture,
+                                               sizeString, jordan)
+                        if 'yeezy' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'Sold Out', ItemLink, ItemPicture,
+                                               sizeString, yeezy)
+                        if 'nike' in ItemName.lower() and 'sb' in ItemName.lower():
+                            SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'Sold Out', ItemLink, ItemPicture,
+                                               sizeString, nikesb)
+                    except:
+                        print('ERROR: Couldn\'t send message. Product Type: '+ product['product_type'].lower())
+                ItemList.pop(index)
+                ItemList.append(temp)
+        else:
+            if not SoldOut:
+                try:
+                    SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'In Stock', ItemLink, ItemPicture, sizeString, webhook)
+                    print('[IN STOCK]' + ItemName)
+                    if 'jordan' in ItemName.lower():
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'In Stock', ItemLink, ItemPicture,
+                                           sizeString, jordan)
+                    if 'yeezy' in ItemName.lower():
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'In Stock', ItemLink, ItemPicture,
+                                           sizeString, yeezy)
+                    if 'nike' in ItemName.lower() and 'sb' in ItemName.lower():
+                        SendDiscordMessage(ItemName, ItemColor, ItemPrice, 'In Stock', ItemLink, ItemPicture,
+                                           sizeString, nikesb)
+                except:
+                    print('ERROR: Couldn\'t send message. Product Type: ' + product['product_type'].lower())
+            ItemList.append(temp)
+
+
+
+    #print(decodedJson['products'][0]['title'])
+    file = open('shoegallery_list.dat', 'wb')
+    pickle.dump(ItemList, file)
+    file.close()
